8924408b4e44ea05ca0432c0709ffbc23ea80be9
Estefanía Santana <83146564+tefsantana@users.noreply.github.com>
2021-07-09 06:18:40 -0300

    Add files via upload

---
+++ SistemaGestion/src/main/java/com/mycompany/sistemagestion/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sistemagestion;
+
+/**
+ *
+ * @author Estefanía
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario(); //guardo el formulario dentro de la variable ventana
+        ventana.show(); //muestra formulario 
+    }
+}
